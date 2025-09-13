0cdf62d6ee9eab7842f4b2024002a80829ed5ef6
yusufcetin-source <yusufcetinforex@gmail.com>
2025-09-13 02:34:51 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>🚀 Starter Project Çalışıyor!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 22,
+    fontWeight: '600',
+  },
+});
